a525afe2b697a0d87fbefbb30beb0592269e2943
Dimitar Dimitrov <me@ddimitrov.name>
2012-08-03 00:17:21 +0300

    Revise Rozalia's solution to hw 02

--- 2012-07-12/solutions/rozalia.rb
+++ 2012-07-12/solutions/rozalia.rb
@@ -1,4 +1,4 @@
-def collection_from_string( songs_as_string, artist_tags )
+def collection_from_string(songs_as_string, artist_tags)
   songs_as_string.split("\n").map do |line|
     {}.tap do |song|
       song[:tags] = []
@@ -6,22 +6,22 @@ def collection_from_string( songs_as_string, artist_tags )
       song[:genre], song[:subgenre] = genres.split(',').map(&:strip)
       song[:tags] = tags.split(',').map(&:strip) if tags
 
-      song[:tags] += (artist_tags[song[:artist]]).uniq if artist_tags.has_key? song[:artist]
+      song[:tags] += artist_tags[song[:artist]].uniq if artist_tags.has_key? song[:artist]
 
-      song[:tags].push(song[:genre].downcase) if song[:genre]
-      song[:tags].push(song[:subgenre].downcase) if song[:subgenre]
+      song[:tags] << song[:genre].downcase if song[:genre]
+      song[:tags] << song[:subgenre].downcase if song[:subgenre]
     end
   end
 end
 
-def find_in_collection( collection, criteria = {} )
+def find_in_collection(collection, criteria = {})
   collection.select do |song|
     criteria.all? do |condition, value|
       if condition == :filter
         value.call(song)
       elsif condition == :tags
         Array(value).all? do |tag|
-          (song[:tags].include? tag) || tag.end_with?('!') && !(song[:tags].include? tag.chop)
+          song[:tags].include?(tag) || tag.end_with?('!') && !song[:tags].include?(tag.chop)
         end
       else
         song[condition] == value
